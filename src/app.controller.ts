e878ff9d942d60c782b82cd58a8e292cf31e3188
Jared Muñoz <amunozbaez669@gmail.com>
2024-09-10 22:10:54 -0600

    feat: add login endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Post, Request, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
+import { AuthService } from './api/auth/services/auth.service';
+import { LocalAuthGuard } from './api/auth/guards/local-auth.guard';
+import { JwtAuthGuard } from './api/auth/guards/jwt-auth.guard';
+import { Public } from './decorators';
 
 @Controller()
 export class AppController {
-  constructor() { }
+
+  constructor(private authService: AuthService) { }
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
 
   @Get('health-check')
   healthCheck(@Res() res: Response) {
