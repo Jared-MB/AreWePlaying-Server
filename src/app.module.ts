10d1c002e0ce0a075dee5a28d5281421f46fec34
Jared Muñoz <amunozbaez669@gmail.com>
2024-09-10 22:11:05 -0600

    fix: add modules to load on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './api/auth/auth.module';
+import { UsersModule } from './api/users/users.module';
+import { UniversityModule } from './api/university/university.module';
+import { TeamsModule } from './api/teams/teams.module';
+import { MatchesModule } from './api/matches/matches.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    UniversityModule,
+    TeamsModule,
+    MatchesModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    })
+  ],
   controllers: [AppController],
   providers: [],
 })
