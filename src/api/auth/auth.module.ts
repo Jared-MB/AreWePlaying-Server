e6d9a1b060366fbb799ca120d356a4cb3340be78
Jared Muñoz <amunozbaez669@gmail.com>
2024-09-10 22:10:29 -0600

    feat: add auth module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './services/auth.service';
+import { UsersModule } from 'src/api/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
-@Module({})
-export class AuthModule {}
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy, {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  },],
+  exports: [AuthService],
+})
+export class AuthModule { }
