22cf976c54d7ebf28c7142e055dac938d33ccd43
Domi13 <marcmontiel98@gmail.com>
2024-09-11 20:53:16 -0600

    sports Services

---
+++ src/api/sport/sport.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../../services/prisma.service';
+import { CreateSportDto } from './dto/create-sport.dto';
+import { UpdateSportDto } from './dto/update-sport.dto';
+
+@Injectable()
+export class SportService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createSport(data: CreateSportDto) {
+    return await this.prisma.sport.create({
+      data,
+    });
+  }
+
+  async getSports() {
+    return await this.prisma.sport.findMany({
+      include: {
+        matches: true,
+      },
+    });
+  }
+
+  async getSportsById(id: number) {
+    const sport = await this.prisma.sport.findUnique({
+      where: { id },
+      include: {
+        matches: true,
+      },
+    });
+    if (!sport) throw new NotFoundException(`Sport with ID ${id} not found`);
+    return sport;
+  }
+
+  async updateSport(id: number, data: UpdateSportDto) {
+    const sport = await this.prisma.sport.update({
+      where: { id },
+      data,
+    });
+    if (!sport) throw new NotFoundException(`Sport with ID ${id} not found`);
+    return sport;
+  }
+
+  async deleteSport(id: number) {
+    const sport = await this.prisma.sport.delete({
+      where: { id },
+    });
+    if (!sport) throw new NotFoundException(`Sport with ID ${id} not found`);
+    return sport;
+  }
+}
--- src/api/teams/teams.module.ts
+++ src/api/teams/teams.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TeamsService } from './teams.service';
+import { TeamsController } from './teams.controller';
+import { PrismaService } from 'src/services';
 
-@Module({})
+@Module({
+  providers: [TeamsService, PrismaService],
+  controllers: [TeamsController],
+})
 export class TeamsModule {}
