9a8d6f8ee34a272455d67e89f8496c5c1ffdcbb8
Domi13 <marcmontiel98@gmail.com>
2024-09-17 14:37:24 -0600

    Matches service & controller

--- src/api/matches/matches.service.ts
+++ src/api/matches/matches.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/services';
+import { CreateMatchtDto } from './dto/create-match.dto';
+import { UpdateMatchDto } from './dto/update-match.dto';
 
 @Injectable()
-export class MatchesService {}
+export class MatchesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createMatch(data: CreateMatchtDto) {
+    return await this.prisma.match.create({
+      data,
+    });
+  }
+
+  async getMatches() {
+    return await this.prisma.match.findMany({});
+  }
+
+  async findOneById(id: number) {
+    const match = await this.prisma.match.findUnique({
+      where: { id },
+    });
+    if (!match) {
+      throw new NotFoundException(`Match with id ${id} not found`);
+    }
+    return match;
+  }
+
+  async updateMatch(id: number, updateMatchDto: UpdateMatchDto) {
+    const match = await this.prisma.match.update({
+      where: { id },
+      data: updateMatchDto,
+    });
+    if (!match) {
+      throw new NotFoundException(`Match with id ${id} not found`);
+    }
+    return match;
+  }
+
+  async remove(id: number) {
+    const match = await this.prisma.match.delete({
+      where: { id },
+    });
+    if (!match) {
+      throw new NotFoundException(`Mathc with id ${id} not found`);
+    }
+    return match;
+  }
+}
