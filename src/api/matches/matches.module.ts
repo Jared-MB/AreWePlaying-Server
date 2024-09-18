362697642424ec1f1deeb3ef2b590d8dae9572b4
Domi13 <marcmontiel98@gmail.com>
2024-09-18 14:22:41 -0600

    providers corregido

--- src/api/matches/matches.module.ts
+++ src/api/matches/matches.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MatchesService } from './matches.service';
 import { MatchesController } from './matches.controller';
+import { PrismaService } from 'src/services';
 
 @Module({
-  providers: [MatchesService],
+  providers: [MatchesService,PrismaService],
   controllers: [MatchesController],
 })
 export class MatchesModule {}
